b2da5616d1b4fb251ced7a980292b04c03b53b55
armandBiro <114163096+armandBiro@users.noreply.github.com>
2022-11-21 15:53:19 +0100

    Update doggo.c

--- doggo.c
+++ doggo.c
@@ -70,31 +70,33 @@ void save_image(pgm img, char* output){
 void brightness(pgm img, float factor){
 	for(int i=0; i<img.h; i++){
 		for(int j=0; j<img.w; j++){
-			img.data[i][j] *= factor;
+			img.data[i][j] = round(img.data[i][j] * factor);
+			if(img.data[i][j] > img.max) img.data[i][j] = img.max;
+			else if(img.data[i][j] < 0) img.data[i][j] = 0;
 		}
 	}
 }
 
 void threshold(pgm img, int threshold){
 	for(int i=0; i<img.h; i++){
-		for(int j=0; j<img.w; i++){
+		for(int j=0; j<img.w; j++){
 			img.data[i][j] = img.data[i][j] > threshold ? img.max : 0;
 		}
 	}
 }
 
-int contrast_transform(int value, int ratio){
+int contrast_transform(int value, int ratio, pgm img){
 	float contrast = pow((100.0 + ratio) / 100.0, 2);
-	value = round(((((value / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+	value = round(((((value / (float)img.max) - 0.5) * contrast) + 0.5) * (float)img.max);
 	if(value < 0) return 0;
-	else if(value > 255) return 255;
+	else if(value > img.max) return img.max;
 	else return value;
 }
 
 void contrast(pgm img, int ratio){
 	for(int i=0; i<img.h; i++){
 		for(int j=0; j<img.w; j++){
-			img.data[i][j] = contrast_transform(img.data[i][j], ratio);
+			img.data[i][j] = contrast_transform(img.data[i][j], ratio, img);
 		}
 	}
 }
@@ -108,16 +110,16 @@ int main(){
 	
 	char choice = 'h';
 	printf("Valassz modositast!\n - (b) fenyero modositasa\n - (c) kontraszt modositasa\n - (t) hatarertek filter\n");
-
+	float factor;
+	int ratio, threshold_ratio;
 	do {
 		
 		scanf(" %c", &choice);
-
-		int factor, ratio, threshold_ratio;
+		
 		switch(choice){
 			case 'b':
 				printf("A modositas merteke (0.0 - 1.0): \n");
-				scanf("%d", &factor);
+				scanf("%f", &factor);
 				brightness(img, factor);
 				break;
 			case 'c':
@@ -134,15 +136,22 @@ int main(){
 				printf("Legyszi letezo modositast valassz!\n");
 				break;
 		}
-	} while(choice != 'b' && choice != 'c' && choice != 't');
+
+	} while (choice != 'b' && choice != 'c' && choice != 't');
 
 	char* out = (char*)malloc(101*sizeof(char));
 	printf("Mi legyen a kimeno fajl neve? (.pgm) \n");
-	scanf(" %s", out);
+	scanf("%s", out);
 	save_image(img, out);
 	free(out);
 
 	free_image(img);
+
+	/*
+	threshold(img, 120);
+	save_image(img, "teszt.pgm");
+	free_image(img);
+	*/
 
 	return 0;
 }
